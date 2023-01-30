79599df018d122da510d1a04e9811a1a8d01b568
Gustavo <gustavo.barahona.i@gmail.com>
2023-01-30 19:03:12 -0300

    Fin sección 11 version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryptjs = require('bcryptjs');
 
 
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async( req, res = response ) => {
 
@@ -33,7 +34,9 @@ const login = async( req, res = response ) => {
             })
         }
         // Genera JWT
-        const token = await generarJWT( usuario.id );
+        console.log('ki wa     Aer');
+        console.log(usuario.id);
+        // const token = await generarJWT( usuario.id );
 
         res.json({
             ok: 'ok',
@@ -47,12 +50,63 @@ const login = async( req, res = response ) => {
             msg: "Hable con el administrdor"
         })
     }
+}
 
-   
+const googleSignIn = async( req, res = response ) => {
+
+    const { id_token } = req.body;
+
+
+    try {
+
+        const { correo, nombre, img } = await googleVerify( id_token );
+        // console.log( correo, nombre, img );
+
+        let usuario = await Usuario.findOne({ correo });
+        if ( !usuario ) {
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true,
+                rol: "USER_ROLE"
+
+            };
+
+            usuario = new Usuario( data );
+            // Graba en base de datos
+            await usuario.save();
+        }
+        // Si el usuario figura como eliminado o bloqueado
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        // Genera JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            msg: 'Token google ok',
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+
 
 }
 
+
 module.exports = {
     login,
+    googleSignIn,
 
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
@@ -15,6 +15,13 @@ router.post('/login',
         validarCampos
     ],
     login );
+
+router.post('/google', 
+    [
+        check('id_token', 'Token de google es necesario').not().isEmpty(),
+        validarCampos
+    ],
+    googleSignIn );
 
 
 module.exports = router
